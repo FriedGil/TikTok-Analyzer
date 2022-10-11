5bea341fa4d6f2b4826b8e76a57ca20f76712a9b
Gil <friedmang@bxscience.edu>
2022-10-10 22:11:12 -0400

    data

--- browser-to-mp3/download.py
+++ browser-to-mp3/download.py
@@ -1,9 +1,13 @@
 import urllib.request
+  
+class Num:
+    num = 0
 
-
+#MAKE SUPPORT path as parameter
 def video_download(url):
     try:
-        urllib.request.urlretrieve(url, "1.mp3")
+        urllib.request.urlretrieve(url, f"audio/{Num.num}.mp3")
+        Num.num += 1
     except Exception as e:
         if e == urllib.error.HTTPError:
             print("Blob")
